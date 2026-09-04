bd35f64bdf539e42ca45d0658ec9315bea54fe30
ShitalThorat304 <shitalthorat874@gmail.com>
2026-09-05 08:30:22 +1000

    remove ui tests

--- tests/ui/UiBasic.spec.js
+++
@@ -1,9 +0,0 @@
-const {test, expect}=require('@playwright/test');
-
-test('Basic Test', async({browser})=> {
-
-   const context =await browser.newContext();
-   const page=await context.newPage();
-   await page.goto("https://www.google.com/");
-   await expect(page).toHaveTitle(/Google/);
-   })
